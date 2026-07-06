ee7660f5c070406731c4bc9033ad1a45d679b237
kristoffer <kristoffer.andergrim@logikfabriken.se>
2026-07-06 16:51:30 +0200

    Fix copy-paste error when applying license.

--- lua/codex_bridge/context.lua
+++ lua/codex_bridge/context.lua
@@ -12,7 +12,9 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.local config = require("codex_bridge.config")
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+local config = require("codex_bridge.config")
 
 local M = {}
 
--- lua/codex_bridge/websocket.lua
+++ lua/codex_bridge/websocket.lua
@@ -12,7 +12,9 @@
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <https://www.gnu.org/licenses/>.local uv = vim.uv or vim.loop
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+local uv = vim.uv or vim.loop
 
 local bit = bit or bit32
 
